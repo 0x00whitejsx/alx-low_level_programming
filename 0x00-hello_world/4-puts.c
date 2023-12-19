39d8794ed3a15abff699917383e95bba917c82de
0x00witejsx <0x00whitejsx@gmail.com>
2023-12-19 11:39:53 -0800

    put functions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+* Files: put.c
+* Auth: joesph Jacob
+*/
+#include <stdio.h>
+
+/*
+*main - print strint
+*Return: always 0.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
