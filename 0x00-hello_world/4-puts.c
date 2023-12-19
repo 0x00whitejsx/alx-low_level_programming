dcdc487f6f3f0bf65e10e52629720f3e5648eef5
0x00witejsx <0x00whitejsx@gmail.com>
2023-12-19 11:44:36 -0800

    put functions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,11 @@
 */
 #include <stdio.h>
 
-/*
-*main - print strint
-*Return: always 0.
+/**
+* main - prints "with proper grammer" strint
+* Return: always 0.
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
