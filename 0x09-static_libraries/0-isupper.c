79709044e53bedaa350e317d3af95bcaaa201068
0x00witejsx <0x00whitejsx@gmail.com>
2024-01-15 06:40:39 -0800

    library created

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-/** _isupper - function that check uppercase character
+/*
+ * _isupper - function that check uppercase character
  * @c: the character to be view
  * Return: success 1 is upper otherwise 0
  */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z')
+	return (c >= 'A' && c <= 'Z');
 }
