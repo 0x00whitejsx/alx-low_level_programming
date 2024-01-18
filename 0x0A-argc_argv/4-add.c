db3ad8016ca7910d1bbd1a322dfc9c8bf954dc45
0x00whitejsx <0x00whitejsx@gmail.com>
2024-01-18 12:18:35 +0100

    Function that add posivtive vlue

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include "main.h"
 
@@ -5,7 +8,7 @@
  * main - Program that takes in all integer arguments and returns the sum
  * @argc: Number of command line arguments
  * @argv: Array name
- * Return: succefully 1 if a non-integer is among the passed in arguments, 0 otherwise
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
 
 int main(int argc, char *argv[])
