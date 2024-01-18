41cb53f5c4334d06fec29685fa426d6aa3c56dc0
0x00whitejsx <0x00whitejsx@gmail.com>
2024-01-18 11:49:06 +0100

    added argc file that reutn the function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - print the name of the programm
+* @argc: number of cammand in line argument
+* @argv: Array input
+* Return: success 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
